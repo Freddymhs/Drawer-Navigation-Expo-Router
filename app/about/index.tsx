a73869253f67af61d70779ca234b628b1c4a711b
@fmarcos <@fmarcos.dev>
2024-12-06 02:06:16 -0300

    entendiendo rutas

--- app/about/index.tsx
+++ app/about/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-export default function Settings() {
+export default function About() {
   return (
     <View>
-      <Text>CONTENIDO PRINCIPAL DE LA APLICACION</Text>
+      <Text>abbbbbooooottt</Text>
     </View>
   );
 }
--- app/home/bebidas.tsx
+++ app/home/bebidas.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-export default function Settings() {
+export default function Bebidas() {
   return (
     <View>
-      <Text>CONTENIDO PRINCIPAL DE LA APLICACION</Text>
+      <Text>CBebidasION</Text>
     </View>
   );
 }
